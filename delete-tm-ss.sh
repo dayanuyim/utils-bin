3005fd7468a1662a1a4e560a8824431e455527c6
dayanuyim <dayanuyim@gmail.com>
2019-12-14 18:38:00 +0800

    Add more scripts
    	new file:   copy-dev
    	new file:   delete-tm-ss.sh
    	new file:   disable-app-recent
    	new file:   ffmpeg-concate
    	new file:   ffmpeg-merge
    	new file:   get-bundle-id
    	new file:   list-listen
    	new file:   mount-ntfs-rw
    	new file:   show-ansi-color-table

---
+++ delete-tm-ss.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! uname -a | grep -q Darwin; then
+    echo "Only for MAC"
+    exit 1
+fi    
+
+# Delete TimeMachine Snapshots
+
+for t in `tmutil listlocalsnapshots / | cut -d"." -f4`
+do
+    tmutil deletelocalsnapshots $t
+done
