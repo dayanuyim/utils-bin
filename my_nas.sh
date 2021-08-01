afa87c4baa06ee36653e276519878e5849bfaf06
dayanuyim <dayanuyim@gmail.com>
2021-08-01 19:22:39 +0800

    [Update] beaglebone
    - mnt devices, esp sdcard

--- my_nas.sh
+++
@@ -1,7 +0,0 @@
-ip=
-name=
-dst="/media/$USER/$name"
-cred="/$HOME/.smbcred"
-
-mkdir -p "$dst"
-sudo mount -t cifs //$ip/$name "$dst" -o vers=1.0,_netdev,credentials=$cred,user,uid=1000,gid=1000,iocharset=utf8,file_mode=0774,dir_mode=0775
