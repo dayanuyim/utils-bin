29fc82950bf9bd478184c07edeb075af2dde7372
dayanuyim <dayanuyim@gmail.com>
2024-03-20 17:02:34 +0800

    Update pdf-bookmarks.sh

--- pdftk-bookmarks.sh
+++ pdftk-bookmarks.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
-srcpdf="$1"
-dstpdf="$2"
 
-pdftk "$srcpdf" \
-    update_info bookmarks.txt \
-    output "$dstpdf"
+pdf="$1"
+bookmarks="$2"
+if [ -z "$pdf" ]; then
+    echo "usage: ${0##*/} <pdf> [bookmarks.txt]" >&2
+    exit 1
+fi
+
+if [ ! -f "$pdf" ]; then
+    echo "pdf not exists" >&2
+    exit 2
+fi
+
+if [ -z "$bookmarks" ]; then
+    bookmarks="$(dirname "$pdf")/pdftk-bookmarks.txt"
+fi
+
+if [ ! -f "$bookmarks" ]; then
+    echo "bookmarks not exists" >&2
+    exit 3
+fi
+
+orig="${pdf%.pdf}.orig.pdf"
+
+mv "$pdf" "$orig" && \
+    pdftk "$orig" update_info "$bookmarks" output "$pdf"
